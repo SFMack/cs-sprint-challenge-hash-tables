c3ec5856dc9a9b2da1bcf0afb2012bfaeb93ece6
SFMack <sfcamack@icloud.com>
2020-11-08 19:47:51 -0800

    first exercise complete. ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,23 @@
-# Your code here
-
-
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    only_files = {}
+    duplicates = {}
+    for f in files:
+        if f.find("/") == -1:
+            only_files[f] = f
+        split_f = f.split("/")
+        last = len(split_f) - 1
+
+        if split_f[last] not in only_files:
+            only_files[split_f[last]] = f
+        else:
+            duplicates[split_f[last]] = f
+    
+    for q in queries:
+        if q in only_files:
+            result.append(only_files[q])
+        if q in duplicates:
+            result.append(duplicates[q])
 
     return result
 
